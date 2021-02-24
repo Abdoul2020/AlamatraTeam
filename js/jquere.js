9adfc69cb35f28ca0b84d3e51aecf771c6183c71
Abdul kowyou <abdoulkowiyy2020@gmail.com>
2021-02-24 21:04:18 +0300

    Alamatra

---
+++ js/jquere.js
@@ -0,0 +1,62 @@
+(function($) {
+
+	"use strict";
+ 
+	
+
+
+  
+      /* --------------------------------------------------- */
+	/*  Vegas Slideshow
+	------------------------------------------------------ */
+	 $("#hero").vegas({
+		transitionDuration: 2000,
+		delay: 15000,
+    	slides: [
+        { src: "images/ro1.gif" },
+       	{ src: "images/ro2.png" },
+         { src: "images/simu1.gif" },
+         { src: "images/simu2.gif" }
+    	],
+        transition: 'slideLeft2',
+	});
+
+  $('#previous').on('click', function () {
+    // $elmt
+    $("#hero").vegas('options', 'transition', 'slideRight2').vegas('previous');
+});
+
+$('#next').on('click', function () {
+  // $elmt.vegas('options', 'transition', 'slideLeft2').vegas('next');
+  //$elmt
+  $("#hero").vegas('options', 'transition','slideLeft2').vegas('next');
+});
+
+ /* --------------------------------------------------- */
+	/*  Vegas Slideshow
+	------------------------------------------------------ */
+    $("#heroes").vegas({
+		
+		transitionDuration: 2000,
+	    
+		delay: 4000,
+    	slides: [
+        { src: "images/ekip45.jpeg" },
+       	{ src: "images/ekip56.jpeg" },
+         { src: "images/ekipyari.jpeg" }
+    	],
+        transition: 'slideLeft2',
+	});
+
+  $('#previous').on('click', function () {
+    // $elmt
+    $("#heroes").vegas('options', 'transition', 'slideRight2').vegas('previous');
+});
+
+$('#next').on('click', function () {
+  // $elmt.vegas('options', 'transition', 'slideLeft2').vegas('next');
+  //$elmt
+  $("#heroes").vegas('options', 'transition','slideLeft2').vegas('next');
+});
+
+})(jQuery);
